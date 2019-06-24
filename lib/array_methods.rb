5739df5cba97a0eeb37f6d7e29892f2b10fda2f4
Alethia Quintero <alethiaq22@gmail.com>
2019-06-24 23:23:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -16,5 +16,9 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.length.times do |index|
+    if array[index] == array.min 
+      return array.min
+    end
+  end
 end
